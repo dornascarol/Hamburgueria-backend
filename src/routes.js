dc85e1288ccfb3c6d2d7259c9a8c6acb403c6952
dornascarol <carolinafdornas@gmail.com>
2023-07-19 19:11:55 -0300

    adicionando dependencia desenv nodemon e sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-const { Router } = require("express")
+import { Router } from "express"
 
 const routes = new Router()
 
@@ -6,4 +6,4 @@ routes.get("/", (request, response) => {
     return response.json({message: "Hello, World"})
 })
 
-module.exports = routes
+export default routes
