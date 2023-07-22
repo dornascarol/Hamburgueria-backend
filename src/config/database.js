d2173870cd923fce83fe18464c401c7b9e97dfd2
dornascarol <carolinafdornas@gmail.com>
2023-07-22 13:04:24 -0300

    iniciando as configuracoes do database

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'postgres',
+    database: 'codeburguer',
+    define: {
+        timespamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+}
