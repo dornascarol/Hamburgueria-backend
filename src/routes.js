6bb4028718a0693d2787427ebe8453c4b94d5b01
dornascarol <carolinafdornas@gmail.com>
2023-07-25 23:48:32 -0300

    configurando o Model de usuario e gravando dados

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello, World' })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Rodolfo',
+    email: 'rodolfo@email.com',
+    password_hash: '23asd234'
+  })
+  return response.json(user)
 })
 
 export default routes
