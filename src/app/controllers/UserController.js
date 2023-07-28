e425427a61fc64b46b6798e06c0b6e126a04b381
dornascarol <carolinafdornas@gmail.com>
2023-07-28 09:08:06 -0300

    criando Hash de senha

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,17 +8,17 @@ class UserController{
         const schema = Yup.object().shape({
             name: Yup.string().required(),
             email: Yup.string().email().required(),
-            password_hash: Yup.string().required().min(6),
+            password: Yup.string().required().min(6),
             admin: Yup.boolean(),
         })
 
         try{
             await schema.validateSync(request.body, { abortEarly: false })
-        }catch(err){
+        } catch(err){
             return response.status(400).json({ error: err.errors })
         }
 
-        const { name, email, password_hash, admin } = request.body
+        const { name, email, password, admin } = request.body
 
         const userExists = await User.findOne({
             where: { email },
@@ -28,13 +28,11 @@ class UserController{
             return response.status(400).json({ error: 'E-mail já cadastrado' })
         }
 
-        console.log(userExists)
-
         const user = await User.create({
             id: v4(),
             name,
             email,
-            password_hash,
+            password,
             admin,
         })
 
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,16 +1,26 @@
 import Sequelize, { Model } from 'sequelize'
+import bcrypt from 'bcrypt'
 
 class User extends Model {
     static init(sequelize) {
         super.init({
             name: Sequelize.STRING,
             email: Sequelize.STRING,
+            password: Sequelize.VIRTUAL,
             password_hash: Sequelize.STRING,
             admin: Sequelize.BOOLEAN,
         },
         {
             sequelize,
         })
+
+        this.addHook('beforeSave', async(user) => {
+            if(user.password){
+                user.password_hash = await bcrypt.hash(user.password, 10)
+            }
+        })
+
+        return this
     }
 }
 
