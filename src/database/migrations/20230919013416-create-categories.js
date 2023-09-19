9c7ecd9b56195d85d23821ca200b4742b10a6f1e
dornascarol <carolinafdornas@gmail.com>
2023-09-18 23:35:51 -0300

    adicionando tabela de categorias

---
+++ src/database/migrations/20230919013416-create-categories.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('categories', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+     });
+     
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('categories');
+     
+  }
+};
