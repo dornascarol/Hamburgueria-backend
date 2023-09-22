4cb8a324a39ebcd03bb7a66229e7f0d022c40790
dornascarol <carolinafdornas@gmail.com>
2023-09-21 21:35:10 -0300

    validando categoria repetida

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -16,9 +16,19 @@ class CategoryController {
 
         const { name } = request.body
 
-        const category = await Category.create({ name });
+        const categoryExists = await Category.findOne({
+            where: {
+                name,
+            },
+        })
 
-        return response.json(category);
+        if(categoryExists){
+            return response.status(400).json({ error: "Categoria já existe"})
+        }
+
+        const { id } = await Category.create({ name });
+
+        return response.json({name, id});
         } catch (err) {
             console.log (err)
         }
