64554ad9972635f1fb4531cd038accea21b72301
dornascarol <carolinafdornas@gmail.com>
2023-09-27 22:00:31 -0300

    adiciona novo produto e altera Controller produtos

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup'
 import Product from '../models/Product'
+import Category from '../models/Category'
 
 class ProductController {
     async store(request, response) {
@@ -7,7 +8,7 @@ class ProductController {
             const schema = Yup.object().shape({
                 name: Yup.string().required(),
                 price: Yup.number().required(),
-                category: Yup.string().required(),
+                category_id: Yup.number().required(),
             })
 
             try {
@@ -17,12 +18,12 @@ class ProductController {
             }
 
             const { filename: path } = request.file
-            const { name, price, category } = request.body
+            const { name, price, category_id } = request.body
 
             const product = await Product.create({
                 name,
                 price,
-                category,
+                category_id,
                 path,
             });
             
@@ -33,7 +34,15 @@ class ProductController {
     }
 
     async index(request, response) {
-        const products = await Product.findAll()
+        const products = await Product.findAll({
+            include: [
+                {
+                    model: Category,
+                    as: 'category',
+                    attributes: ['id', 'name'],
+            },
+        ],
+        })
 
         return response.json(products)
     }
