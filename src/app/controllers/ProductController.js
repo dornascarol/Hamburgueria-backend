dad9c0f0f6d8aafded2a5a0725a53ae85d6e3acb
dornascarol <carolinafdornas@gmail.com>
2023-11-15 21:22:04 -0300

    validando usuario admin em produto

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup'
 import Product from '../models/Product'
 import Category from '../models/Category'
+import User from '../models/User'
 
 class ProductController {
     async store(request, response) {
@@ -15,6 +16,12 @@ class ProductController {
                 await schema.validateSync(request.body, { abortEarly: false })
             } catch (err) {
                 return response.status(400).json({ error: err.errors })
+            }
+
+            const { admin:isAdmin } = await User.findByPk(request.userId)
+
+            if (!isAdmin) {
+               return response.status(401).json()
             }
 
             const { filename: path } = request.file
