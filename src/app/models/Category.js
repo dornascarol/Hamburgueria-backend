a1549a6b2f51f11ea9cea89e25fd8c2cacdccd1a
dornascarol <carolinafdornas@gmail.com>
2023-11-26 00:14:15 -0300

    adicionando path em category do Model

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -5,6 +5,13 @@ class Category extends Model {
         super.init(
             {
             name: Sequelize.STRING,
+            path: Sequelize.STRING,
+            url: {
+                type:Sequelize.VIRTUAL,
+                get() {
+                    return `http://localhost:3000/category-file/${this.path}`
+                },
+            },
         },
         {
             sequelize,
