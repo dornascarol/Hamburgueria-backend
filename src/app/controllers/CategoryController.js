c53719c70b8a7f38e67622d3ef1e67dabac572ef
dornascarol <carolinafdornas@gmail.com>
2023-12-10 13:39:36 -0300

    acrescenta file em upload e metodo update em category Controller

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -48,6 +48,48 @@ class CategoryController {
 
         return response.json(category)
     }
+
+    async update(request, response) {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string(),
+        })
+
+        try {
+            await schema.validateSync(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
+        }
+
+        const { admin:isAdmin } = await User.findByPk(request.userId)
+
+        if (!isAdmin) {
+            return response.status(401).json()
+        }
+
+        const { name } = request.body
+        
+        const { id } = request.params
+
+        const category = await Category.findByPk(id)
+
+        if(!category){
+            return response.status(401).json({ error: "Verifique se o ID da sua categoria está correto"})
+        }
+
+        let path 
+        if(request.file){
+            path = request.file.filename
+        }
+
+        await Category.update({ name, path })
+
+        return response.status(200).json();
+        } catch (err) {
+            console.log (err)
+        }
+    }
+
 }
 
 export default new CategoryController()
